114e69872d589b8b1c7e26af48b77e56615f4857
Stijn Verhoeven <stijn.verhoeven@liantis.be>
2024-06-14 13:42:48 +0000

    update naming

--- blob/variables.tf
+++ blob/variables.tf
@@ -24,7 +24,6 @@ variable "env_type" {
 }
 
 locals {
-  storage_account_container_prefix = "c-"
   resource_suffix                  = module.humanitecvars.humanitec_env_type_resource_suffix_mapping[var.env_type]
 
   subscription_id = module.humanitecvars.subscription_ids[local.resource_suffix]
@@ -32,5 +31,5 @@ locals {
 
   workload_name = replace(trimprefix(var.res_id, "modules."), "/.external.*/", "")
 
-  name = lower(substr("${local.storage_account_container_prefix}${var.app_id}-${var.env_id}-${local.workload_name}", 0, 62))
+  name = lower(substr("${module.humanitecvars.azure_resource_prefixes["storage_container"]}${var.app_id}-${var.env_id}-${local.workload_name}", 0, 62))
 }
