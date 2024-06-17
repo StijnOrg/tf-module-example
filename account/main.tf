dc0dc9ae7f8fc39e80f28170fb6101bc491937a5
Stijn Verhoeven <stijn.verhoeven@liantis.be>
2024-06-17 10:04:29 +0000

    cleanup unused module

--- account/main.tf
+++ account/main.tf
@@ -2,8 +2,3 @@ module "humanitecvars" {
   source = "github.com/liantisit-iac-modules/humanitecvars?ref=v1"
   humanitec_resource_suffix = local.resource_suffix
 }
-
-module "naming" {
-  source  = "Azure/naming/azurerm"
-  version = "~> 0.4"
-}
